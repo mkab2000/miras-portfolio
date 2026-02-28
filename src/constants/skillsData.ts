8cc5d851ae19bdf13e18ddecbf81a22ccd3e4352
Miras Kabdygali <miras.kab@gmail.com>
2026-02-28 12:23:24 +0900

    updated skills

--- src/constants/skillsData.ts
+++ src/constants/skillsData.ts
@@ -3,22 +3,38 @@ import iconJavaScript from "../assets/icons/icons8-javascript-100.png";
 import iconCSS from "../assets/icons/icons8-css-100.png";
 import iconRedux from "../assets/icons/icons8-redux-100.png";
 import iconTypeScript from "../assets/icons/icons8-typescript-100.png";
-import iconReact from "../assets/icons/icons8-react-native-100.png";
+import iconReact from "../assets/icons/icons8-react-100.png";
+import iconReactNative from "../assets/icons/icons8-react-native-100.png";
 import iconPhotoshop from "../assets/icons/icons8-photoshop-100.png";
 import iconAudacity from "../assets/icons/icons8-audacity-100.png";
 import iconMuseScore from "../assets/icons/icons8-musescore-100.png";
 import iconNextJs from "../assets/icons/icons8-next-100.png"
 import iconSupabase from "../assets/icons/icons8-supabase-100.png"
+import iconAws from "../assets/icons/icons8-aws-96.png"
+import iconAndroid from "../assets/icons/icons8-android-studio-100.png"
+import iconXCode from "../assets/icons/icons8-xcode-100.png"
+import iconSql from "../assets/icons/icons8-sql-100.png"
+import iconPostgreSql from "../assets/icons/icons8-postgresql-100.png"
+import iconDocker from "../assets/icons/icons8-docker-100.png"
+import iconVercel from "../assets/icons/icons8-vercel-100.png"
 
 export const skillsData = [
   { label: "HTML", icon: iconHTML },
   { label: "JavaScript", icon: iconJavaScript },
   { label: "CSS", icon: iconCSS },
   { label: "React", icon: iconReact },
+  { label: "React Native", icon: iconReactNative },
   { label: "Redux", icon: iconRedux },
   { label: "Typescript", icon: iconTypeScript },
   { label: "NextJs", icon: iconNextJs },
+  { label: "SQL", icon: iconSql },
+  { label: "PostgreSQL", icon: iconPostgreSql },
+  { label: "Docker", icon: iconDocker },
+  { label: "Vercel", icon: iconVercel },
+  { label: "AWS", icon: iconAws },
   { label: "Supabase", icon: iconSupabase },
+  { label: "Android Studio", icon: iconAndroid },
+  { label: "XCode", icon: iconXCode },
   { label: "Photoshop", icon: iconPhotoshop },
   { label: "Audacity", icon: iconAudacity },
   { label: "MuseScore", icon: iconMuseScore },
